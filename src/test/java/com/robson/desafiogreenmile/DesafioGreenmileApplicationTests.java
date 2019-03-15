ce687ebf1d7cb48c8c5d9b5791442a9672c351b4
robsonsilv4 <robsonsilv410@gmail.com>
2019-03-15 19:58:48 -0300

    Testes para resource de usuário

--- src/test/java/com/robson/desafiogreenmile/DesafioGreenmileApplicationTests.java
+++ src/test/java/com/robson/desafiogreenmile/DesafioGreenmileApplicationTests.java
@@ -3,14 +3,16 @@ package com.robson.desafiogreenmile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DesafioGreenmileApplication.class)
+@TestPropertySource("classpath:application.properties")
 public class DesafioGreenmileApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
