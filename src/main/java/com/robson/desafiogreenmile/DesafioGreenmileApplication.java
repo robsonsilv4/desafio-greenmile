4beb1262576bdd34b689f3b3faa0f96beeac7d7e
robsonsilv4 <robsonsilv410@gmail.com>
2019-03-15 12:45:32 -0300

    Projeto inicial

---
+++ src/main/java/com/robson/desafiogreenmile/DesafioGreenmileApplication.java
@@ -0,0 +1,13 @@
+package com.robson.desafiogreenmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGreenmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGreenmileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robson/desafiogreenmile/DesafioGreenmileApplicationTests.java
@@ -0,0 +1,16 @@
+package com.robson.desafiogreenmile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioGreenmileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
