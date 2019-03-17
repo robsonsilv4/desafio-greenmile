9cfe79ae132d3be8ea988e67056141dcd9448aef
robsonsilv4 <robsonsilv410@gmail.com>
2019-03-16 21:28:38 -0300

    Liberado cors e exposto location

--- src/main/java/com/robson/desafiogreenmile/configuration/SecurityConfiguration.java
+++ src/main/java/com/robson/desafiogreenmile/configuration/SecurityConfiguration.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -54,8 +56,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+    configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    source.registerCorsConfiguration("/**", configuration);
+
     return source;
   }
 
